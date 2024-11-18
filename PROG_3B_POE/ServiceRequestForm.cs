e747e6d7e239e15020d97ce520b0e6d5022ce5a3
st10068763 <anterozacarias007@gmail.com>
2024-11-18 23:04:41 +0200

    Adding comments to the code.

--- PROG_3B_POE/ServiceRequestForm.cs
+++ PROG_3B_POE/ServiceRequestForm.cs
@@ -12,6 +12,9 @@ namespace PROG_3B_POE
 {
     public partial class ServiceRequestForm : Form
     {
+        /// <summary>
+        /// Class to represent a service request
+        /// </summary>
         public class ServiceRequest : IComparable<ServiceRequest>
         {
             public Guid RequestId { get; set; }
@@ -20,7 +23,11 @@ namespace PROG_3B_POE
             public string Description { get; set; }
             public string Category { get; set; }
             public DateTime RequestDate { get; set; }
-
+            /// <summary>
+            /// This method is used to compare two service requests by their RequestId
+            /// </summary>
+            /// <param name="other"></param>
+            /// <returns></returns>
             public int CompareTo(ServiceRequest other)
             {
                 return RequestId.CompareTo(other.RequestId);
@@ -33,6 +40,9 @@ namespace PROG_3B_POE
         /// <typeparam name="T"></typeparam>
         public class BinarySearchTree<T> where T : IComparable<T>
         {
+            /// <summary>
+            /// This class represents a node in the binary tree
+            /// </summary>
             public class Node
             {
                 public T Data;
@@ -40,64 +50,105 @@ namespace PROG_3B_POE
 
                 public Node(T data) => Data = data;
             }
-
+            /// <summary>
+            /// the root node of the binary tree
+            /// </summary>
             public Node Root;
-
+            /// <summary>
+            /// this method inserts a new node into the binary tree
+            /// </summary>
+            /// <param name="data"></param>
             public void Insert(T data) => Root = Insert(Root, data);
-
+            /// <summary>
+            /// This method inserts a new node into the binary tree
+            /// </summary>
+            /// <param name="node"></param>
+            /// <param name="data"></param>
+            /// <returns></returns>
             private Node Insert(Node node, T data)
             {
+                // here we check if the node is null, if it is we create a new node with the data
                 if (node == null) return new Node(data);
 
                 if (data.CompareTo(node.Data) < 0)
                     node.Left = Insert(node.Left, data);
                 else if (data.CompareTo(node.Data) > 0)
                     node.Right = Insert(node.Right, data);
-
+                // if the data is equal to the node data we return the node
                 return node;
             }
-
+            /// <summary>
+            /// the search method searches for a node in the binary tree
+            /// </summary>
+            /// <param name="data"></param>
+            /// <returns></returns>
             public T Search(T data) => Search(Root, data).Data;
-
+            /// <summary>
+            /// This method searches for a node in the binary tree
+            /// </summary>
+            /// <param name="node"></param>
+            /// <param name="data"></param>
+            /// <returns></returns>
             private Node Search(Node node, T data)
             {
+                // if the node is null or the data is equal to the node data we return the node
                 if (node == null || node.Data.CompareTo(data) == 0)
                     return node;
-
+                // if the data is less than the node data we search the left node
                 return data.CompareTo(node.Data) < 0
                     ? Search(node.Left, data)
                     : Search(node.Right, data);
             }
-
+            /// <summary>
+            /// This method traverses the binary tree in order
+            /// </summary>
+            /// <returns></returns>
             public List<T> InOrderTraversal()
             {
                 var result = new List<T>();
                 InOrderTraversal(Root, result);
                 return result;
             }
-
+            /// <summary>
+            /// used to traverse the binary tree in order
+            /// </summary>
+            /// <param name="node"></param>
+            /// <param name="result"></param>
             private void InOrderTraversal(Node node, List<T> result)
             {
+                // if the node is null we return
                 if (node == null) return;
-
+                // we traverse the left node, add the data to the result list, then traverse the right node
                 InOrderTraversal(node.Left, result);
                 result.Add(node.Data);
                 InOrderTraversal(node.Right, result);
             }
         }
 
+        /// <summary>
+        /// Used to store the service requests
+        /// </summary>
         private BinarySearchTree<ServiceRequest> bst = new BinarySearchTree<ServiceRequest>();
-
+        /// <summary>
+        /// Class to store the status of a service request
+        /// </summary>
         private static class Status
         {
             public const string Pending = "Pending";
             public const string InProgress = "In Progress";
             public const string Resolved = "Resolved";
         }
-
+        /// <summary>
+        /// Used to store the categories of a service request
+        /// </summary>
         private SortedSet<ServiceRequest> requestQueue;
+        /// <summary>
+        /// report issue form
+        /// </summary>
         ReportIssueForm reportIssueForm = new ReportIssueForm();
-
+        /// <summary>
+        /// this method initializes the service request form
+        /// </summary>
         public ServiceRequestForm()
         {
             // Initialize binary search tree and load data before initializing components
@@ -108,7 +159,9 @@ namespace PROG_3B_POE
             DisplayTreeData();
             DisplayChart();
         }
-
+        /// <summary>
+        /// Method to load the issues from the ReportIssueForm into the binary search tree
+        /// </summary>
         private void LoadIssues()
         {
             foreach (var issue in ReportIssueForm.issues)
@@ -117,13 +170,17 @@ namespace PROG_3B_POE
                 bst.Insert(request);
             }
         }
-
+        /// <summary>
+        /// Method to display the service requests in the DataGridView
+        /// </summary>
         private void DisplayTreeData()
         {
             dgvServiceRequests.DataSource = bst.InOrderTraversal();
         }
-        
-        // Custom comparer for sorting requests by priority
+
+        /// <summary>
+        /// Custom comparer to sort service requests by priority
+        /// </summary>
         public class RequestPriorityComparer : IComparer<ServiceRequest>
         {
             public int Compare(ServiceRequest x, ServiceRequest y)
@@ -133,7 +190,11 @@ namespace PROG_3B_POE
             }
         }
 
-        // Convert an IssueReport to a ServiceRequest with priority determination
+        /// <summary>
+        /// Convert an IssueReport to a ServiceRequest with priority determination
+        /// </summary>
+        /// <param name="issue"></param>
+        /// <returns></returns>
         private ServiceRequest ConvertIssueToRequest(ReportIssueForm.IssueReport issue)
         {
             return new ServiceRequest
@@ -194,20 +255,31 @@ namespace PROG_3B_POE
         {
             if (btnLineGraph.Checked) DisplayChart();
         }
-
+        /// <summary>
+        /// This method is used to display the chart based on the radio button selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnStakedGraph_CheckedChanged(object sender, EventArgs e)
         {
             if (btnStakedGraph.Checked) DisplayChart();
         }
-
+        /// <summary>
+        /// This method is used to display the chart based on the radio button selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnStaked_CheckedChanged(object sender, EventArgs e)
         {
             if (btnStaked.Checked) DisplayChart();
         }
 
-
+        /// <summary>
+        /// Display the chart based on the selected chart type
+        /// </summary>
         private void DisplayChart()
         {
+            // Check if the chart type is selected
             if (cbChartType.SelectedItem == null)
             {
                 cbChartType.SelectedIndex = 0;
@@ -236,13 +308,13 @@ namespace PROG_3B_POE
             if (btnLineGraph.Checked) series.ChartType = SeriesChartType.Spline;
             if (btnStakedGraph.Checked) series.ChartType = SeriesChartType.StackedColumn;
             if (btnStaked.Checked) series.ChartType = SeriesChartType.Area;
-
+            // Add the series to the chart
             ServiceChart.Series.Add(series);
 
             // Populate data based on the selected type
             string chartType = cbChartType.SelectedItem.ToString();
             var data = bst.InOrderTraversal();
-
+            // Populate the chart based on the selected chart type
             if (chartType == "Category")
             {
                 var categoryGroups = data.GroupBy(r => r.Category ?? "Unknown")
@@ -276,15 +348,20 @@ namespace PROG_3B_POE
         }
 
         //*************************************END OF CHART*******************************************
-
+        /// <summary>
+        /// button to track the request
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnTrackRequest_Click_1(object sender, EventArgs e)
         {
+            // Check if the user has entered a valid Request ID
             if (Guid.TryParse(txtRequestID.Text, out Guid requestId))
             {
                 // Search for the request in the binary search tree
                 var request = bst.InOrderTraversal()
                     .FirstOrDefault(r => r.RequestId == requestId);
-
+                // Display the request details if found
                 if (request != null)
                 {
                     MessageBox.Show($"Request ID: {request.RequestId}\n" +
@@ -295,11 +372,13 @@ namespace PROG_3B_POE
                     // Highlight the tracked request in the DataGridView
                     HighlightTrackedRequest(requestId);
                 }
+                // Display an error message if the request is not found
                 else
                 {
                     MessageBox.Show("Request not found.");
                 }
             }
+            // Display an error message if the user enters an invalid Request ID
             else
             {
                 MessageBox.Show("Please enter a valid Request ID.");
@@ -307,11 +386,18 @@ namespace PROG_3B_POE
         }
 
         //-----------------------------------Update request methods
+        /// <summary>
+        /// Button to update the status of a request
+        /// </summary>
+        /// <param name="requestId"></param>
+        /// <param name="newStatus"></param>
         private void UpdateRequestStatus(Guid requestId, string newStatus)
         {
+            // Search for the request in the binary search tree
             var request = bst.Search(new ServiceRequest { RequestId = requestId });
             if (request != null)
             {
+                // Update the status of the request
                 request.Status = newStatus;
                 DisplayTreeData();
                 DisplayChart();
@@ -319,8 +405,14 @@ namespace PROG_3B_POE
         }
 
         //----------------------------Search method
+        /// <summary>
+        /// Button to search for service requests based on user inputs
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnSearch_Click_1(object sender, EventArgs e)
         {
+            // Check if there are any service requests to search
             if (bst == null || !bst.InOrderTraversal().Any())
             {
                 MessageBox.Show("No service requests available to search.");
@@ -328,9 +420,9 @@ namespace PROG_3B_POE
             }
 
             // Get user inputs
-            string searchQuery = txtSearch.Text.Trim().ToLower(); // Convert to lowercase for case-insensitive search
+            string searchQuery = txtSearch.Text.Trim().ToLower(); 
             string selectedCategory = txtSearch.Text.Trim().ToLower();
-            string selectedStatus = txtSearch.Text.Trim().ToLower(); // Selected status (if any)
+            string selectedStatus = txtSearch.Text.Trim().ToLower(); 
 
             // Perform the search using LINQ
             var results = bst.InOrderTraversal().Where(request =>
@@ -347,18 +439,23 @@ namespace PROG_3B_POE
             // Display the results
             if (results.Any())
             {
-                dgvServiceRequests.DataSource = results; // Bind results to DataGridView
+                dgvServiceRequests.DataSource = results; 
             }
             else
             {
                 MessageBox.Show("No matching service requests found.");
-                dgvServiceRequests.DataSource = null; // Clear DataGridView if no results
+                dgvServiceRequests.DataSource = null; 
             }
         }
 
-
+        /// <summary>
+        /// Button to reset all filters and display all service requests
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnReset_Click(object sender, EventArgs e)
         {
+            // Check if there are any service requests to reset
             if (bst == null || !bst.InOrderTraversal().Any())
             {
                 MessageBox.Show("No service requests available to reset.");
@@ -373,7 +470,6 @@ namespace PROG_3B_POE
             rdBtnMediumPriority.Checked = false;
             rdBtnHighPriority.Checked = false;
             rdBtnLowPriority.Checked = false;
-
             // Reload all service requests into the DataGridView
             dgvServiceRequests.DataSource = bst.InOrderTraversal().ToList();
             MessageBox.Show("Filters reset. Displaying all service requests.");
@@ -387,10 +483,11 @@ namespace PROG_3B_POE
         /// <param name="priority">The priority to filter by.</param>
         private void FilterByPriority(string priority)
         {
+            // Check if there are any service requests to filter
             var filteredRequests = bst.InOrderTraversal()
                 .Where(request => request.Priority == priority)
                 .ToList();
-
+            // Display the filtered requests
             if (filteredRequests.Any())
             {
                 dgvServiceRequests.DataSource = filteredRequests;
@@ -398,7 +495,8 @@ namespace PROG_3B_POE
             else
             {
                 MessageBox.Show($"No service requests found with priority: {priority}");
-                dgvServiceRequests.DataSource = null; // Clear if no matches
+                // Clear if no matches found
+                dgvServiceRequests.DataSource = null;
             }
         }       
 
@@ -409,6 +507,7 @@ namespace PROG_3B_POE
         /// <param name="e"></param>
         private void cbCategorySorting_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // Check if there are any service requests to sort
             if (cbCategorySorting.SelectedIndex != -1)
             {
                 string selectedCategory = cbCategorySorting.SelectedItem.ToString();
@@ -427,7 +526,11 @@ namespace PROG_3B_POE
                 }
             }
         }
-
+        /// <summary>
+        /// This method is used to display the chart based on the radio button selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void dgvServiceRequests_CellValueChanged_1(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex >= 0 && dgvServiceRequests.Columns[e.ColumnIndex].Name == "StatusColumn")
@@ -447,7 +550,10 @@ namespace PROG_3B_POE
                 }
             }
         }
-
+        /// <summary>
+        /// Method to highlight the tracked request in the DataGridView
+        /// </summary>
+        /// <param name="requestId"></param>
         private void HighlightTrackedRequest(Guid requestId)
         {
             foreach (DataGridViewRow row in dgvServiceRequests.Rows)
@@ -460,7 +566,11 @@ namespace PROG_3B_POE
                 }
             }
         }
-
+        /// <summary>
+        /// button to filter the requests by priority, High Priority
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void rdBtnHighPriority_CheckedChanged(object sender, EventArgs e)
         {
             if (rdBtnHighPriority.Checked)
@@ -468,7 +578,11 @@ namespace PROG_3B_POE
                 FilterByPriority("High Priority");
             }
         }
-
+        /// <summary>
+        /// Button to filter the requests by priority, Medium Priority
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void rdBtnMediumPriority_CheckedChanged(object sender, EventArgs e)
         {
             if (rdBtnMediumPriority.Checked)
@@ -476,7 +590,11 @@ namespace PROG_3B_POE
                 FilterByPriority("Medium Priority");
             }
         }
-
+        /// <summary>
+        /// button to filter the requests by priority, Low Priority
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void rdBtnLowPriority_CheckedChanged(object sender, EventArgs e)
         {
             if (rdBtnLowPriority.Checked)
@@ -484,9 +602,14 @@ namespace PROG_3B_POE
                 FilterByPriority("Low Priority");
             }
         }
-
+        /// <summary>
+        /// Button to export the service requests to a CSV file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnExportData_Click(object sender, EventArgs e)
         {
+            // Check if there is any data to export
             if (dgvServiceRequests.Rows.Count == 0)
             {
                 MessageBox.Show("No data to export.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -514,7 +637,11 @@ namespace PROG_3B_POE
                 }
             }
         }
-
+        /// <summary>
+        /// Method to export the DataGridView to a CSV file
+        /// </summary>
+        /// <param name="dgv"></param>
+        /// <param name="filePath"></param>
         private void ExportDataGridViewToCSV(DataGridView dgv, string filePath)
         {
             using (StreamWriter writer = new StreamWriter(filePath))
